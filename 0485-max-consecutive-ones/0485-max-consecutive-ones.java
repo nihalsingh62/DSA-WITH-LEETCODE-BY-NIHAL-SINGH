0a1e2545cfd5688a5ac9ad6da1ea6385c9de7ef3
NIHAL KUMAR <nihal62096@gmail.com>
2026-08-05 01:17:23 +0530

    Time: 4 ms (4.72%), Space: 52.4 MB (66.8%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,14 +3,18 @@ class Solution {
         return loopd(nums);
         
     }static int loopd(int[] nums){
-        int max  = Integer.MIN_VALUE;
+        int left = 0;
         int sum = 0;
-        for(int i = 0;i<nums.length;i++){
-            sum += nums[i]; 
-            max =  Math.max(max,sum);
-            if(nums[i] == 0){
-                sum = 0;
-            } 
+        int max = Integer.MIN_VALUE;
+        
+        while(left!=nums.length){
+            if(nums[left] != 0){
+                sum += nums[left];
+                left++;
+            }else if(nums[left] == 0){
+                left++;
+                sum=0;
+            }max = Math.max(sum,max);
         }return max;
     }
 }
